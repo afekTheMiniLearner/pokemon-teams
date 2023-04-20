7173890be20a2a3f4d7b320ba84aecdcd9ec6307
afekTheMiniLearner <afeksa2000@gmail.com>
2023-04-21 00:50:12 +0300

    renamed main.scss to index.scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import "./main.scss";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
+
+import "./index.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
