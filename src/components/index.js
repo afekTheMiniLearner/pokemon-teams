7293d695b8421f779fceecacbcafca9f0ed75525
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-04-23 16:24:23 +0300

    created const of data types to use as my group of toggle buttons

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export { default as ToggleButtons } from "./ToggleButtons/ToggleButtons";
+export { default as ToggleDataButtons } from "./ToggleDataButtons/ToggleDataButtons";
---
+++ src/utils/consts.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const TOGGLE_DATA_OPTIONS = ["Main", "Advanced"];
