3cbb033dfa70fcc5f05fc66648174d2c5526c55f
afekTheMiniLearner <afeksa2000@gmail.com>
2023-04-29 23:33:30 +0300

    fixed size issue

--- src/base-components/Card/Card.styled.jsx
+++ src/base-components/Card/Card.styled.jsx
@@ -3,6 +3,6 @@ import Card from "@mui/material/Card";
 
 // eslint-disable-next-line import/prefer-default-export
 export const MuiCard = styled(Card)`
-  max-height: 280px;
-  min-height: 280px;
+  height: 280px;
+  width: 400px;
 `;
