b51450291b1592e4f9c3d6d2c5c844529c1a3817
afekTheMiniLearner <afeksa2000@gmail.com>
2023-04-29 14:38:40 +0300

    export PokemonChips index file

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as ToggleDataButtons } from "./ToggleDataButtons/ToggleDataButtons";
 export { default as GamesList } from "./GamesList/GamesList";
 export { default as PokemonCard } from "./PokemonCard/PokemonCard";
+export { default as PokemonChips } from "./PokemonChips/PokemonChips";
