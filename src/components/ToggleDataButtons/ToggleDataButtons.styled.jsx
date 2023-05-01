d7a9704a8a66a52b3f896ffcb808ddbffe81f1ef
afekTheMiniLearner <afeksa2000@gmail.com>
2023-05-01 23:02:47 +0300

    fixed bug of toggle team buttons falling down when there is not team present in the page

--- src/components/ToggleDataButtons/ToggleDataButtons.styled.jsx
+++ src/components/ToggleDataButtons/ToggleDataButtons.styled.jsx
@@ -3,8 +3,12 @@ import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import ToggleButton from "@mui/material/ToggleButton";
 
 export const MuiToggleButtonGroup = styled(ToggleButtonGroup)`
-  margin-left: auto;
+  position: absolute;
+  top: 0;
+  right: 0;
   z-index: 1;
+  margin-top: 100px;
+  margin-right: 100px;
 `;
 
 export const MuiToggleButton = styled(ToggleButton)``;
