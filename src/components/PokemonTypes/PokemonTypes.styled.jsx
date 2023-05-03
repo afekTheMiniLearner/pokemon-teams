ceaac1437a0d8fa7e3aeaccab755f2136b4418fe
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-05-03 15:05:13 +0300

    added 1px gap

--- src/components/PokemonTypes/PokemonTypes.styled.jsx
+++ src/components/PokemonTypes/PokemonTypes.styled.jsx
@@ -4,10 +4,13 @@ import MuiBox from "@mui/material/Box";
 import MuiAvatar from "@mui/material/Avatar";
 
 export const Box = styled(MuiBox)`
+  display: flex;
+  flex-direction: column;
   position: absolute;
   margin: 2px;
   right: 0;
   top: 0;
+  gap: 1px;
 `;
 
 export const Zoom = styled(MuiZoom)`
