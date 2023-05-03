13798ffe61f3a5eea55bef938f6887828fd0bc32
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-05-03 14:10:45 +0300

    deleted import of grow in app becuase have no use there

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Grow from "@mui/material/Grow";
 
 import { PokemonCardsBox, ToggleDataButtons } from "@components";
 import { Title } from "@base-components";
