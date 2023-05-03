29a2c4b02ff7e8a501318d0a2bfd6a55ffa0f484
afekTheMiniLearner <afeksacajo@gmail.com>
2023-05-03 17:19:19 +0300

    changed from display flex the style of the Grid

--- src/base-components/ChipsBox/ChipsBox.styled.jsx
+++ src/base-components/ChipsBox/ChipsBox.styled.jsx
@@ -3,9 +3,7 @@ import { styled } from "@mui/material/styles";
 import MuiZoom from "@mui/material/Zoom";
 import MuiGrid from "@mui/material/Grid";
 
-export const Grid = styled(MuiGrid)`
-  display: flex;
-`;
+export const Grid = styled(MuiGrid)``;
 
 export const Zoom = styled(MuiZoom)`
   transition-delay: 200ms;
