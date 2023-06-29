e830225f73b1145aa920601f4c344d8ea79c1cae
afekTheMiniLearner <afeksa2000@gmail.com>
2023-06-29 16:56:10 +0300

    changed MQ to mui responsive in Chip styled file

--- src/base-components/Chip/Chip.styled.jsx
+++ src/base-components/Chip/Chip.styled.jsx
@@ -2,28 +2,17 @@ import { styled } from "@mui/material/styles";
 import Chip from "@mui/material/Chip";
 
 // eslint-disable-next-line import/prefer-default-export
-export const MuiChip = styled(Chip)`
-  display: flex !important;
-  justify-content: center !important;
-  align-items: center !important;
+export const MuiChip = styled(Chip)(({ theme }) => ({
+  display: "flex !important",
+  justifyContent: "center !important",
+  alignItems: "center !important",
+  fontSize: "0.75em",
+  fontWeight: "bolder",
 
-  @media (min-width: 1025px) and (max-width: 1280px) {
-    font-weight: bolder;
-    font-size: 0.75em;
-  }
-
-  @media (min-width: 768px) and (max-width: 1024px) {
-    font-weight: bolder;
-    font-size: 0.6em;
-  }
-
-  @media (min-width: 481px) and (max-width: 767px) {
-    font-weight: bolder;
-    font-size: 0.5em;
-  }
-
-  @media (max-width: 480px) {
-    font-weight: bolder;
-    font-size: 0.48em;
-  }
-`;
+  [theme.breakpoints.down("md")]: {
+    fontSize: "0.6em",
+  },
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "0.56em",
+  },
+}));
