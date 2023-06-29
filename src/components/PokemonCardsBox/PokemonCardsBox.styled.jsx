3011d8da6def9cf8292392578551c2cc54014c32
afekTheMiniLearner <afeksa2000@gmail.com>
2023-06-29 17:07:53 +0300

    changed MQ to mui responsive in PokemonCardsBox  styled file

--- src/components/PokemonCardsBox/PokemonCardsBox.styled.jsx
+++ src/components/PokemonCardsBox/PokemonCardsBox.styled.jsx
@@ -4,18 +4,18 @@ import MuiBox from "@mui/material/Box";
 
 import MyPokemonCard from "../PokemonCard/PokemonCard";
 
-export const Box = styled(MuiBox)`
-  display: flex;
-  justify-content: space-evenly;
-  flex-wrap: wrap;
-  margin-top: 150px;
-  gap: 20px;
+export const Box = styled(MuiBox)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-evenly",
+  flexWrap: "wrap",
+  marginTop: "150px",
+  gap: "20px",
 
-  @media (max-width: 480px) {
-    position: absolute;
-    right: 10px;
-    top: 110px;
-  }
-`;
+  [theme.breakpoints.down("xs")]: {
+    position: "absolute",
+    right: "10px",
+    top: "110px",
+  },
+}));
 
 export const PokemonCard = styled(MyPokemonCard)``;
