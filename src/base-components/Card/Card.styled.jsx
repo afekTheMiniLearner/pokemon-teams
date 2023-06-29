81929f559bda596b226e02940011d4b9859ffb71
afekTheMiniLearner <afeksa2000@gmail.com>
2023-06-29 16:52:18 +0300

    changed MQ to mui responsive in Card styled file and added specific theme to adjust the mui responsive breakpoints

--- src/base-components/Card/Card.styled.jsx
+++ src/base-components/Card/Card.styled.jsx
@@ -7,89 +7,74 @@ import MuiTypography from "@mui/material/Typography";
 import MuiGrow from "@mui/material/Grow";
 import MuiBox from "@mui/material/Box";
 
-export const MuiCard = styled(Card)`
-  display: flex;
-  height: 310px;
-  width: 370px;
-  margin: 0 20px;
+export const MuiCard = styled(Card)(({ theme }) => ({
+  display: "flex",
+  height: "310px",
+  width: "370px",
+  margin: "0 20px",
 
-  @media (min-width: 1025px) and (max-width: 1280px) {
-    height: 260px;
-    width: 300px;
-    margin: 0 10px;
-  }
-
-  @media (min-width: 768px) and (max-width: 1024px) {
-    height: 230px;
-    width: 260px;
-    margin: 0 5px;
-  }
-
-  @media (min-width: 510px) and (max-width: 767px) {
-    height: 230px;
-    width: 240px;
-    margin: 0;
-  }
-
-  @media (max-width: 509px) {
-    height: 210px;
-    width: 230px;
-    margin: 0;
-  }
-`;
+  [theme.breakpoints.down("lg")]: {
+    height: "260px",
+    width: "300px",
+    margin: "0 10px",
+  },
+  [theme.breakpoints.down("md")]: {
+    height: "230px",
+    width: "260px",
+    margin: "0 5px",
+  },
+  [theme.breakpoints.down("sm")]: {
+    height: "230px",
+    width: "240px",
+    margin: 0,
+  },
+  [theme.breakpoints.down("xs")]: {
+    height: "210px",
+    width: "230px",
+    margin: 0,
+  },
+}));
 
 export const CardContent = styled(MuiCardContent)``;
 
-export const CardMedia = styled(MuiCardMedia)`
-  height: 150px;
-  object-fit: contain;
+export const CardMedia = styled(MuiCardMedia)(({ theme }) => ({
+  height: "150px",
+  objectFit: "contain",
 
-  @media (min-width: 1025px) and (max-width: 1280px) {
-    height: 120px;
-  }
-
-  @media (min-width: 768px) and (max-width: 1024px) {
-    height: 100px;
-  }
-
-  @media (min-width: 481px) and (max-width: 767px) {
-    height: 90px;
-  }
-
-  @media (max-width: 480px) {
-    height: 80px;
-  }
-`;
+  [theme.breakpoints.down("lg")]: {
+    height: "120px",
+  },
+  [theme.breakpoints.down("md")]: {
+    height: "90px",
+  },
+  [theme.breakpoints.down("sm")]: {
+    height: "80px",
+  },
+}));
 
 export const CardActionArea = styled(MuiCardActionArea)``;
 
-export const TitleTypography = styled(MuiTypography)`
-  @media (min-width: 1025px) and (max-width: 1280px) {
-    font-size: 1.2em;
-  }
+export const TitleTypography = styled(MuiTypography)(({ theme }) => ({
+  fontSize: "1.2em",
 
-  @media (min-width: 768px) and (max-width: 1024px) {
-    font-size: 1.1em;
-  }
+  [theme.breakpoints.down("md")]: {
+    fontSize: "1.1em",
+  },
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "1em",
+  },
+}));
 
-  @media (max-width: 767px) {
-    font-size: 1em;
-  }
-`;
+export const ContentTypography = styled(MuiTypography)(({ theme }) => ({
+  fontSize: "0.8em",
 
-export const ContentTypography = styled(MuiTypography)`
-  @media (min-width: 1025px) and (max-width: 1280px) {
-    font-size: 0.8em;
-  }
-
-  @media (min-width: 768px) and (max-width: 1024px) {
-    font-size: 0.7em;
-  }
-
-  @media (max-width: 767px) {
-    font-size: 0.6em;
-  }
-`;
+  [theme.breakpoints.down("md")]: {
+    fontSize: "0.7em",
+  },
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "0.6em",
+  },
+}));
 
 export const Grow = styled(MuiGrow)``;
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import App from "./App";
 import "./index.scss";
 
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 509,
+      sm: 767,
+      md: 1024,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
