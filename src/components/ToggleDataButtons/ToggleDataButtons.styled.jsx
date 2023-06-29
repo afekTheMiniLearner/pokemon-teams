9556a9609cbf99ec0a0529cb554f426e0abcac03
afekTheMiniLearner <afeksa2000@gmail.com>
2023-06-29 17:41:08 +0300

    .

--- src/components/ToggleDataButtons/ToggleDataButtons.styled.jsx
+++ src/components/ToggleDataButtons/ToggleDataButtons.styled.jsx
@@ -2,57 +2,48 @@ import { styled } from "@mui/material/styles";
 import MuiToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import MuiToggleButton from "@mui/material/ToggleButton";
 
-export const ToggleButtonGroup = styled(MuiToggleButtonGroup)`
-  position: absolute;
-  top: 25px;
-  right: 30px;
-  z-index: 1;
-  margin-top: 100px;
+export const ToggleButtonGroup = styled(MuiToggleButtonGroup)(({ theme }) => ({
+  position: "absolute",
+  top: "25px",
+  right: "30px",
+  zIndex: 1,
+  marginTop: "100px",
 
-  @media (min-width: 1280px) and (max-width: 1470px) {
-    margin-top: 0;
-    top: unset;
-    bottom: 15px;
-    right: 30px;
-  }
+  [theme.breakpoints.down("lg")]: {
+    top: "unset",
+    bottom: "15px",
+    right: "30px",
+    marginTop: "100px",
+  },
+  [theme.breakpoints.down("md")]: {
+    right: "20px",
+  },
+  [theme.breakpoints.down("sm")]: {
+    bottom: "10px",
+    right: "5px",
+  },
+  [theme.breakpoints.down("xs")]: {
+    position: "absolute",
+    bottom: "unset",
+    right: "unset",
+    top: "70px",
+  },
+}));
 
-  @media (min-width: 1025px) and (max-width: 1280px) {
-    margin-top: 0;
-    top: unset;
-    bottom: 15px;
-    right: 30px;
-  }
+export const ToggleButton = styled(MuiToggleButton)(({ theme }) => ({
+  fontSize: "0.5em",
+  height: "40px",
+  width: "130px",
 
-  @media (max-width: 1024px) {
-    margin-top: 0;
-    top: unset;
-    bottom: 15px;
-    right: 20px;
-  }
-
-  @media (min-width: 481px) and (max-width: 767px) {
-    bottom: 10px;
-    right: 5px;
-  }
-
-  @media (max-width: 480px) {
-    bottom: unset;
-    right: unset;
-    top: 175px;
-  }
-`;
-
-export const ToggleButton = styled(MuiToggleButton)`
-  @media (min-width: 1025px) and (max-width: 1280px) {
-    font-size: 13px;
-    height: 30px;
-    width: 110px;
-  }
-
-  @media (max-width: 1024px) {
-    font-size: 10px;
-    font-weight: bolder;
-    height: 25px;
-    width: 95px;
-  }
-`;
+  [theme.breakpoints.down("lg")]: {
+    fontSize: "13px",
+    height: "30px",
+    width: "110px",
+  },
+  [theme.breakpoints.down("md")]: {
+    fontSize: "10px",
+    fontWeight: "bolder",
+    height: "25px",
+    width: "95px",
+  },
+}));
